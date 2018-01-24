da392adaa95cf99d99da2739e73cd8fce7dcc5a9
florafeng <sfeng3@ualberta.ca>
2018-01-23 18:49:19 -0700

    lab2 clear button implemented

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -44,6 +44,8 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
+
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +56,20 @@ public class LonelyTwitterActivity extends Activity {
 
 				Tweet tweet = new NormalTweet(text);
 				tweetList.add(tweet);
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
+			}
+		});
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				String text = bodyText.getText().toString();
+
+				tweetList.clear();
 
 				adapter.notifyDataSetChanged();
 
